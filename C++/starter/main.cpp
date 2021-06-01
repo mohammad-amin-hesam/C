5e15d40ea7959881d84f4b59f6dd0bb0f25ba67c
Amin <singlepageprogrammer@gmail.com>
2021-06-01 17:52:15 +0430

    amin: C++ starter

---
+++ C++/starter/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+// main() is where program execution begins.
+int main()
+{
+  cout << "Hello World"; // prints Hello World
+  cout << "Hi there";
+  return 0;
+}
